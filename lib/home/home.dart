9f8fac161dc1c9a9c01a119bee1b17b155016376
ldr <catzilla312@gmail.com>
2022-10-04 18:16:23 +0600

    adding sevices

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:fireapp/login/login.dart';
+import 'package:fireapp/topics/topics.dart';
+import 'package:fireapp/services/auth.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text('About'),
-          onPressed: () => Navigator.pushNamed(context, '/topics'),
-        ),
-      ),
+    return StreamBuilder(
+      builder: AuthService().userStream,
     );
   }
 }
